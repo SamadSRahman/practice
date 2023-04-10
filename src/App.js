3f30d4bed25eff129c92983e6b6b87b3eaf26033
SamadSRahman <samadsrahman@gmail.com>
2023-04-11 00:09:59 +0530

    New branch with changes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,66 @@
-
-import './App.css';
-import { useState } from 'react';
-
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [log, setLog] = useState([])
-  const [data, setData] = useState({ elementName: "",
-  eventName: "",
-  time:""
-  })
-  const date = new Date()
-  const showTime = date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()+ "\n" +
-       date.getHours() 
-        + ':' + date.getMinutes() 
-    + ":" + date.getSeconds();
-  
+  const [log, setLog] = useState([]);
+  const [data, setData] = useState({
+    elementName: "",
+    eventName: "",
+    time: "",
+  });
+  const date = new Date();
+  const showTime =
+    date.getDate() +
+    "/" +
+    (date.getMonth() + 1) +
+    "/" +
+    date.getFullYear() +
+    "\n" +
+    date.getHours() +
+    ":" +
+    date.getMinutes() +
+    ":" +
+    date.getSeconds();
+
   function handleMouseOver(ele) {
     setData({
       elementName: ele,
-      eventName: "handleMouseOver",
-      time:showTime
-    })
-    setLog([...log, data])
+      eventName: "MouseOver",
+      time: showTime,
+    });
+    setLog([...log, data]);
   }
   function handleOnMouseLeave(ele) {
     setData({
       elementName: ele,
-      eventName: "handleMouseLeave",
-      time:showTime
-    })
-    setLog([...log, data])
+      eventName: "MouseLeave",
+      time: showTime,
+    });
+    setLog([...log, data]);
   }
   return (
     <div className="App">
-      <div className='elementsContainer'>
-     <h1 onMouseOver={()=>handleMouseOver("h1")} onMouseLeave={()=>handleOnMouseLeave("h1")}>Hi I am heading 1</h1>
-      <h2 onMouseOver={()=>handleMouseOver("h2")} onMouseLeave={()=>handleOnMouseLeave("h2")}>Hi I am heading 2</h2>
-      <div onMouseOver={()=>handleMouseOver("div")} onMouseLeave={()=>handleOnMouseLeave("div")}>I am div</div>
+      <div className="elementsContainer">
+        <h1
+          onMouseOver={() => handleMouseOver("h1")}
+          onMouseLeave={() => handleOnMouseLeave("h1")}
+        >
+          Hi I am heading 1
+        </h1>
+        <h2
+          onMouseOver={() => handleMouseOver("h2")}
+          onMouseLeave={() => handleOnMouseLeave("h2")}
+        >
+          Hi I am heading 2
+        </h2>
+        <div
+          onMouseOver={() => handleMouseOver("div")}
+          onMouseLeave={() => handleOnMouseLeave("div")}
+        >
+          I am div
+        </div>
       </div>
-      <div className='tableContainer'>
+      <div className="tableContainer">
         <table>
           <thead>
             <tr>
@@ -53,12 +75,12 @@ function App() {
                 <td>{ele.time}</td>
                 <td>{ele.eventName}</td>
                 <td>{ele.elementName}</td>
-            </tr>
-          ))}
+              </tr>
+            ))}
           </tbody>
-      </table>
+        </table>
       </div>
-      </div>
+    </div>
   );
 }
 
