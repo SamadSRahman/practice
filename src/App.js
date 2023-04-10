3cdbe98dad0a798d5d401e400bd69f938c7beb14
SamadSRahman <samadsrahman@gmail.com>
2023-04-10 21:07:54 +0530

    Assignment 3 Greetings pushed to Repository

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const greeting = ["Good Morning", "Good Afternoon", "Good Evening", "Good Night"]
+  const [count, setCount] = useState(0)
+  function handleOnClick(){
+    if(count>=greeting.length-1){
+      setCount(0)
+    }
+    else
+    setCount(count+1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{greeting[count]}</h1>
+      <button onClick={handleOnClick}>Change Greeting</button>
+     
     </div>
   );
 }
