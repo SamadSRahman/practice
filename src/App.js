94583b4adc1c215696970da145b9458c9c2751c3
SamadSRahman <121732577+SamadSRahman@users.noreply.github.com>
2023-05-03 03:24:12 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import {BsSearch} from 'react-icons/bs'
 
 function App() {
+  const [input, setInput] = useState("")
+  const [list, setList] = useState([])
+  const [selected, setSelected] = useState("")
+  const [isVisible, setIsVisible] = useState(false)
+  let search =[]
+  const fruits = ["banana", "orange", "apple", "mango", "grapes", "pineapple", "jackfruit", "dragonfruit", "lichi", "dates", "papaya", "watermelon", "melon", "green apple", "guava"];
+  function handleChange(e) {
+    setInput(e.target.value)
+  
+    search = fruits.filter((ele) => {
+      const temp = ele;
+      if (temp.includes(input)) {
+        return temp;
+      }
+    });
+
+    setList(search);
+    setIsVisible(true)
+  }
+  function handleClick(fruit) {
+    const temp = fruits.find((ele) => ele === fruit)
+    setSelected(temp)
+    setIsVisible(!isVisible)
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+    <div className='container'>
+      <div className='searchBar'>
+        
+        <label><BsSearch/></label>
+        <input onChange={(e) => handleChange(e)} />
+     
+       
+      </div >
+      {list.map((ele,index) => (
+        <div className='content' style={isVisible ? {} : { display:'none'}}>
+           <span onClick={()=>handleClick(ele)} >{ele}</span>
+       </div>
+      ))}
+        </div>
+      <div className='outerDiv'>
+        <h3>Selcted Fruit: {selected}</h3>
     </div>
+    </div>
+    
   );
 }
 
 export default App;
+
---
+++ src/test.js
@@ -0,0 +1,2 @@
+const string = "1 2 3 4 5"
+const arr = string
